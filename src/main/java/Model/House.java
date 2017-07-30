0ef593ba967e29959349470372bdaed52208c25e
HenokZewdie <javabcamp@gmail.com>
2017-07-30 09:59:07 -0400

    The Begining, begins

---
+++ src/main/java/Model/House.java
@@ -0,0 +1,158 @@
+package Model;
+
+import javax.persistence.*;
+import java.util.Date;
+
+/**
+ * Created by student on 6/21/17.
+ */
+@Entity
+@Table(name = "house")
+public class House {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String street;
+
+    private String city;
+
+    private String state;
+
+    private long zipCode;
+
+    private String type;
+
+    private String bath;
+
+    private String kitchen;
+
+    private String duration;
+
+    private double price;
+    private long phone;
+    private String remark;
+    @Temporal (TemporalType.DATE)
+    private Date date;
+    private String username;
+    private String image;
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public long getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(long zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getBath() {
+        return bath;
+    }
+
+    public void setBath(String bath) {
+        this.bath = bath;
+    }
+
+    public String getKitchen() {
+        return kitchen;
+    }
+
+    public void setKitchen(String kitchen) {
+        this.kitchen = kitchen;
+    }
+
+    public String getDuration() {
+        return duration;
+    }
+
+    public void setDuration(String duration) {
+        this.duration = duration;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public long getPhone() {
+        return phone;
+    }
+
+    public void setPhone(long phone) {
+        this.phone = phone;
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}
---
+++ src/main/java/rentbet/demo/RentBetApplication.java
@@ -0,0 +1,12 @@
+package rentbet.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentBetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentBetApplication.class, args);
+	}
+}
